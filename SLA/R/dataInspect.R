a310e3bc15f33d11681890238fd907836dc8162a
gustavosdo <gustavo.if.ufrj@gmail.com>
2019-11-10 17:25:43 -0300

    Cleaning older chunks of code

--- SLA/R/dataInspect.R
+++ SLA/R/dataInspect.R
@@ -36,37 +36,14 @@ dataInspect = function(fileName = NULL){
   notNullSolved = notNullDate[notNullDate$callStatus %in%c('N0', 'N4', 'CV'),]
 
   # Converting date time format
-  charDateTime = gsub ( x = gsub(x = as.character(notNullSolved$closeDateTime), pattern = 'ISODate', replacement = '') , replacement = '', pattern = "\"")
+  charDateTime = gsub(x = gsub(x = as.character(notNullSolved$closeDateTime), pattern = 'ISODate', replacement = '') , replacement = '', pattern = "\"")
   charDateTime = gsub(x = charDateTime, pattern = "\\(", replacement = '')
   charDateTime = gsub(x = charDateTime, pattern = "\\)", replacement = '')
   charDateTime = gsub(x = charDateTime, pattern = "T", replacement = ' ')
   notNullSolved$closeDateTime = charDateTime
 
-  # Checking the daylight saving time entries
-  # for (iter_n in 1:dim(notNullSolved)[1]){
-  #   iter_row = notNullSolved[iter_n,]
-  #   charTime = iter_row$closeDateTime
-  #   if (substr(x = charTime, start = 26, stop = 26) == '2'){
-  #     print(charTime)
-  #   }
-  # }
-
   # Slicing the strings of closeDateTime
   notNullSolved$closeDateTime = sapply(notNullSolved$closeDateTime, function(x){substr(x = x, start = 1, stop = 19)})
-
-  # # Converting dates to "numeric" format
-  # threads = parallel::detectCores() - 1
-  # cl = makeCluster(threads)
-  # registerDoParallel(cl)
-  #
-  # charDateTime = foreach::foreach (iter_n = 1:dim(notNullSolved)[1]) %dopar% {
-  #   return(strptime(x = notNullSolved$closeDateTime[iter_n], format = "%Y-%m-%dT%H:%M:%S"))
-  # }
-  # notNullSolved$closeDateTime = charDateTime
-  # stopCluster(cl)
-
-  # Histogram of closeDateTime
-  #hist(as.POSIXct(preProcessedData$closeDateTime), "weeks", format = "%d %b")
 
   return(notNullSolved)
 }
