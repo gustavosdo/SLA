22a9d56266791fb57ce9f0e81bcfb64104624edf
gustavosdo <gustavo.if.ufrj@gmail.com>
2019-11-10 23:05:38 -0300

    Fixing filename path, change histogram to plot by day

--- SLA/run.R
+++ SLA/run.R
@@ -2,6 +2,6 @@ rm(list=ls())
 gc()
 .rs.restartR()
 devtools::load_all()
-fileName = '/home/luga/Dropbox/Git/DataScientistTest/dataset/ticket_cientista.csv'
+fileName = '../dataset/ticket_cientista.csv'
 preProcessedData = dataInspect(fileName = fileName)
-hist(as.POSIXct(preProcessedData$closeDateTime), "weeks", format = "%d %b")
+hist(as.POSIXct(preProcessedData$closeDateTime), "days", format = "%d %b")
