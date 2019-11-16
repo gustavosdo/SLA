5483e483c0f871bdcc1ed9d9971546c3e0c7d403
gustavosdo <gustavo.if.ufrj@gmail.com>
2019-11-16 01:50:46 -0300

    Fixing if's in SLA.R; adding cfg parameters in convertDate.R

--- SLA/R/convertDate.R
+++ SLA/R/convertDate.R
@@ -2,10 +2,13 @@
 convertDate = function(dataset, cfg){
 
   # Only not null close date time entries
-  dataset = dataset[dataset$closeDateTime != 'null',]
+  colTime = cfg$pre_process$closeDate_col
+  dataset = dataset[dataset[colTime] != 'null',]
+
+  ##### APPLY PIPE IN THE GSUB'S ---------
 
   # Converting date time format
-  charDateTime = gsub(x = gsub(x = as.character(dataset$closeDateTime), pattern = 'ISODate', replacement = '') , replacement = '', pattern = "\"")
+  charDateTime = gsub(x = gsub(x = as.character(dataset[,colTime]), pattern = 'ISODate', replacement = '') , replacement = '', pattern = "\"")
   charDateTime = gsub(x = charDateTime, pattern = "\\(", replacement = '')
   charDateTime = gsub(x = charDateTime, pattern = "\\)", replacement = '')
   charDateTime = gsub(x = charDateTime, pattern = "T", replacement = ' ')
@@ -14,7 +17,10 @@ convertDate = function(dataset, cfg){
   charDateTime = sapply(charDateTime, function(x){substr(x = x, start = 1, stop = 19)})
 
   # returns the harmonized date/time set to original dataset
-  dataset$closeDateTime = charDateTime
+  dataset[colTime] = charDateTime
+
+
+  ###### FIX HERE! ----
 
   # removes the entry solved from 2017
   dataset = dataset[order(dataset$closeDateTime),]
