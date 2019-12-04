cb959c1abbbb86732f02fb32dd652d96e98fd0f6
Luiz Gustavo Silva de Oliveira <gustavo.if.ufrj@gmail.com>
2019-12-04 12:47:06 -0300

    Fixing sla for all customers. Package fully working.

--- SLA/R/calcSLA.R
+++ SLA/R/calcSLA.R
@@ -21,19 +21,19 @@ calcSLA = function(dataset, cfg){
 
   # Customer codes
   customerCodes = as.character(unique(dataset[,cfg$pre_process$customers_col]))
-  customerCodes[(length(customerCodes) + 1)] = ''
+  customerCodes[(length(customerCodes) + 1)] = 'all'
 
   # Calculate SLA for each user using threads
   customersData = foreach (iter_name = customerCodes[1:length(customerCodes)], .packages = c('devtools')) %dopar% {
     load_all()
     customerData = as.list(NA)
-    names(customerData) = ifelse( nchar(iter_name) > 0, iter_name, 'all' )
-    datasetCustomer = ifelse( nchar(iter_name) > 0, dataset[dataset[cfg$pre_process$customers_col] == as.numeric(iter_name),], dataset)
-    if (nchar(customerData) > 0){
-      customerData[[iter_name]] = calcCustomerSLA(datasetCustomer, cfg)
+    names(customerData) = iter_name
+    if(iter_name != 'all'){
+      datasetCustomer = dataset[dataset[cfg$pre_process$customers_col] == as.numeric(iter_name),]
     } else {
-      customerData[['all']] = calcCustomerSLA(datasetCustomer, cfg)
+      datasetCustomer = dataset
     }
+    customerData[[iter_name]] = calcCustomerSLA(datasetCustomer, cfg)
     return(customerData)
   }
   return(customersData)
