1032dc1312e226cae412c27a7c544fd0f4d8f170
gustavosdo <gustavo.if.ufrj@gmail.com>
2019-12-23 21:28:46 -0300

    Adding input layer size (reading length of columns in dataset).

--- SLA/R/setupNN.R
+++ SLA/R/setupNN.R
@@ -5,7 +5,7 @@
 #' @param config_json_filename A json with configuration data
 #'
 
-setupNN = function(cfg, train_dataset){
+setupNN = function(cfg, train_train_dataset){
 
   # Neural network parameters
   N_hidden = cfg$process$neural_network$hidden_layers
@@ -13,8 +13,13 @@ setupNN = function(cfg, train_dataset){
   loss = cfg$process$neural_network$loss_function
   neurons = cfg$process$neural_network$neurons_per_hidden_layer
 
-  # Neurons in input layer are one for each parameter on dataset
-  neurons_t0 =
+  # Target and preview parameters columns
+  target_column = cfg$process$neural_network$target_column
+  param_columns = names(train_dataset)[!(names(train_dataset) %in% target_column)]
+  if ("X" %in% param_columns) {param_columns = param_columns[-which("X" %in% param_columns)]}
+
+  # Neurons in input layer are one for each parameter on train_dataset
+  neurons_t0 = length(param_columns)
 
   # Returning the trained neural network
   return(NN_trained)
