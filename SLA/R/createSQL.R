d6e0e804646783fe16229a6fa20af47c21093b43
gustavosdo <gustavo.if.ufrj@gmail.com>
2020-02-09 18:31:09 -0300

    Adding database folder entry in JSON. Starting to write createSQL function

--- SLA/R/createSQL.R
+++ SLA/R/createSQL.R
@@ -1,8 +1,23 @@
+#' @title A SQL with the json configuration
+#' @name createSQL
+#'
+#' @description This function is used in order to create a SQL database
+#' using the jsoin configuration input
+#'
+#' @param cfg The json configuration file read by jsonlite::fromJSON function
+#'
+#' @return A SQL database address (it saves the SQL too)
+#'
+#' @import RSQLite DBI
 
-library(RSQLite)
-
-createSQL(cfg){
+createSQL = function(cfg) {
 
   # SQL with cfg ---------------------------------------------------------------
+  dbDriver = SQLite()
+  con = dbConnect(dbDriver, ":memory:") #dbname = paste0(cfg$folders$database, "cfg"))
+  dbWriteTable(conn = con, "cfg", 3)
+  dbListTables(con)
 
+  # Closing connection ---------------------------------------------------------
+  dbDisconnect(con)
 }
